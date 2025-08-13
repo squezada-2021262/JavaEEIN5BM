b28b0abdd04fb88c1d813cca865cf31939353bda
squezada-2021262 <squezada-2021262@kinal.edu.gt>
2025-08-12 20:42:29 -0600

    feat: Se agrega proyecto ya con el Validar

--- KinalToys-WebApp/src/java/config/Conexion.java
+++ KinalToys-WebApp/src/java/config/Conexion.java
@@ -1,19 +1,29 @@
-
 package config;
-
+ 
 import com.mysql.jdbc.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+ 
 public class Conexion {
     Connection conexion;
-    
     public Connection Conexion() {
         try {
-            Class.forName("com.mysql.jdbc.Drive");
-            conexion = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/DB_KinalToys?useSSL=false", "root", "admin");
-        } catch(ClassNotFoundException | SQLException e) {
-        }
+           Class.forName("com.mysql.jdbc.Driver");
+           String url = "jdbc:mysql://localhost:3306/DB_KinalToys?useSSL=false&allowPublicKeyRetrieval=true";
+           String user = "quintom";
+           String password = "admin";
+           conexion = (Connection) DriverManager.getConnection(url, user, password);
+       } catch (ClassNotFoundException error) {
+           StackTraceElement elemento = error.getStackTrace()[0];
+           System.out.println("Error en: " + elemento.getClassName() + " linea " + elemento.getLineNumber());
+           System.out.println("Mensaje: " + error.getMessage());
+           error.printStackTrace();
+       } catch (SQLException error) {
+           StackTraceElement elemento = error.getStackTrace()[0];
+           System.out.println("Error en: " + elemento.getClassName() + " linea " + elemento.getLineNumber());
+           System.out.println("Mensaje: " + error.getMessage());
+           error.printStackTrace();
+       }
         return conexion;
     }
 }
--- KinalToys-WebApp/src/java/controlador/Controlador.java
+++ KinalToys-WebApp/src/java/controlador/Controlador.java
@@ -1,15 +1,13 @@
-
 package controlador;
-
+ 
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+ 
 public class Controlador extends HttpServlet {
-
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -19,8 +17,15 @@ public class Controlador extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+            String menu = request.getParameter("menu");
+            String accion = request.getParameter("accion");
+            if (menu.equals("Principal")) {
+            request.getRequestDispatcher("principal-admin.jsp").forward(request, response);
+        }
+
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -34,45 +39,75 @@ public class Controlador extends HttpServlet {
             out.println("</body>");
             out.println("</html>");
         }
-    }
+    }   // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
+
      * Handles the HTTP <code>GET</code> method.
+
      *
+
      * @param request servlet request
+
      * @param response servlet response
+
      * @throws ServletException if a servlet-specific error occurs
+
      * @throws IOException if an I/O error occurs
+
      */
+
     @Override
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
 
+            throws ServletException, IOException {
+
+        processRequest(request, response);
+
+    }
+ 
     /**
+
      * Handles the HTTP <code>POST</code> method.
+
      *
+
      * @param request servlet request
+
      * @param response servlet response
+
      * @throws ServletException if a servlet-specific error occurs
+
      * @throws IOException if an I/O error occurs
+
      */
+
     @Override
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
+
             throws ServletException, IOException {
+
         processRequest(request, response);
+
     }
-
+ 
     /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
+     * Returns a short description of the servlet.
+
+     *
+
+     * @return a String containing servlet description
+
+     */
+
+    @Override
+
+    public String getServletInfo() {
+
+        return "Short description";
+
+    }// </editor-fold>
 }
